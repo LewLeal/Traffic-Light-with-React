defd0876edfcef4c4b4dee0ecaae02f1001246c0
lewleal <lewleal2012@gmail.com>
2021-08-30 17:26:22 -0400

    Traffic Ligh Listo/HooksReactJs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("red")
+  const [color, setColor] = useState("red");
 
   return (
     <div className="container">
       <div className="row">
         <div className="App">
-        <div className="title">
-          <h1>Traffic-Ligth desde ReactJS</h1>
-        </div>
+          <div className="title">
+            <h1>Traffic-Ligth desde ReactJS</h1>
+          </div>
           <div className="block offset-5">
             <div className="lights">
               <div className="red light">
-                <div  id="red" className={"red" === color ? "on" : ""}
-                  onClick={() => setColor("red")} >
-
-                </div>
+                <div
+                  id="red"
+                  className={"red" === color ? "on" : ""}
+                  onClick={() => setColor("red")}
+                ></div>
               </div>
               <div className="yellow light">
-                <div type="button" id="yellow" className={"yellow" === color ? "on" : ""}
-                  onClick={() => setColor("yellow")}>
-
-                </div>
+                <div
+                  type="button"
+                  id="yellow"
+                  className={"yellow" === color ? "on" : ""}
+                  onClick={() => setColor("yellow")}
+                ></div>
               </div>
               <div className="green light">
-                <div type="button" id="green" className={"green" === color ? "on" : ""}
-                  onClick={() => setColor("green")} >
-
-                </div>
+                <div
+                  type="button"
+                  id="green"
+                  className={"green" === color ? "on" : ""}
+                  onClick={() => setColor("green")}
+                ></div>
               </div>
             </div>
           </div>
